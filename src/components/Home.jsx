092c2f5dfd8d7bc9e86e1c60ca2028c9d453f429
aaron toliver <flux.charlotte@gmail.com>
2019-12-29 15:51:05 -0500

    coming soon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,22 +2,17 @@ import React, { Component } from 'react';
 
 // Components \\
 import Header from './Header';
-import Services from './Services';
-import Works from './Works';
-import Blog from './Blog';
-import Contact from './Contact';
-import Footer from './Footer';
+// import Services from './Services';
+// import Works from './Works';
+// import Blog from './Blog';
+// import Contact from './Contact';
+// import Footer from './Footer';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <Header />
-                <Services />
-                <Works />
-                <Blog />
-                <Contact />
-                <Footer />
             </div>
         )
     }
